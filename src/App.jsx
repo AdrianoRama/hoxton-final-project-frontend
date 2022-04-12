bc01c07f1d7761f2e3ea18e3fc4920365c72ca27
AdrianoRama <ramaadriano@outlook.com>
2022-04-12 23:30:37 +0200

    Added welcome page, join and login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Join from './Components/Join/Join'
+import LogIn from './Components/LogIn/LogIn'
 import WelcomePage from './Components/WelcomePage/WelcomePage'
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route index element={<Navigate replace to={'/welcome'} />} />
         <Route path='/welcome' element={<WelcomePage />} />
+        <Route path='/join' element={<Join />} />
+        <Route path='/login' element={<LogIn />} />
       </Routes>
     </div>
   )
