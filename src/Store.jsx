8c44b1061447163b30898b9d1f8a4698c19c85f5
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-04-13 11:27:44 +0200

    Sign in

---
+++ src/Store.jsx
@@ -0,0 +1,41 @@
+import create from 'zustand'
+
+
+export const useStore = create((set, get) => ({
+    user: null,
+    logIn: (email, password) => {
+        fetch('http://localhost:4001/sign-in', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    })
+        .then(resp => resp.json())
+        .then(data => {
+            if (data.error) {
+                alert(data.error)
+            } else {
+                localStorage.token = data.token
+                set({ user: data.user})
+            }
+        })
+    },
+    validate: () => {
+        if (localStorage.token) {
+            fetch('http://localhost:4001/validate', {
+                headers: {
+                    Authorization: localStorage.token
+                }
+            })
+                .then(resp => resp.json())
+                .then(data => {
+                    if (data.error) {
+                        alert('Validation failed.')
+                    } else {
+                        set({ user: data })
+                    }
+                })
+        }
+    }
+}))
