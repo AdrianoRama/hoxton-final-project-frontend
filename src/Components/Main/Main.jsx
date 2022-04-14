56f8448b0a4262f883e9c8c8e387cfe4d5883c53
AdrianoRama <ramaadriano@outlook.com>
2022-04-14 17:22:29 +0200

    Finished Main

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './main.css'
 import { useStore } from "../../Store"
 import { AddBox } from '@material-ui/icons'
+import Masonry from 'react-masonry-css'
 
 
 export default function Main() {
@@ -26,19 +27,29 @@ export default function Main() {
         }
     }, [])
 
+    const breakpoints = {
+        default: 5,
+        1100: 2,
+        700: 1
+    }
 
     return (
         <div className='app__main'>
-            {images?.map(image => (
-                <>
-                    <div className="app__main-img">
-                        <img key={image.id} src={image.link} alt="" />
-                        <div className="app__main-save">
-                            <AddBox style={{ fontSize: 50 }} className='addBox' />
+            <Masonry
+                breakpointCols={breakpoints}
+                className="my-masonry-grid"
+                columnClassName="my-masonry-grid_column">
+                {images?.map(image => (
+                    <>
+                        <div className="app__main-img">
+                            <img key={image.id} src={image.link} alt="" />
+                            <div className="app__main-save">
+                                <AddBox style={{ fontSize: 50 }} className='addBox' />
+                            </div>
                         </div>
-                    </div>
-                </>
-            ))}
+                    </>
+                ))}
+            </Masonry>
         </div>
     )
 }
