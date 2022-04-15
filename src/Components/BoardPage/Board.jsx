400b2ea94990589446728c3f67066a7562b5627f
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-04-15 14:28:53 +0200

    css change

--- src/Components/BoardPage/Board.jsx
+++ src/Components/BoardPage/Board.jsx
@@ -1,3 +1,4 @@
+import { Add, AddAlertOutlined } from '@material-ui/icons';
 import './Board.css';
 
 export default function Board() {
@@ -6,7 +7,8 @@ export default function Board() {
         <div className="board__container">
             <div className="board_box">
                 <div className="board_addBoard">
-                    <svg fill="currentColor" height="71" viewBox="0 0 69 71" width="69"><path d="M29.4846 71H39.5154V40.3966H69V30.6035H39.5154V0H29.4846V30.6035H0V40.3966H29.4846V71Z" fill="currentColor"></path></svg>
+                    {/* <svg fill="currentColor" height="71" viewBox="0 0 69 71" width="69"><path d="M29.4846 71H39.5154V40.3966H69V30.6035H39.5154V0H29.4846V30.6035H0V40.3966H29.4846V71Z" fill="currentColor"></path></svg> */}
+                    <Add className='plus' style={{ fontSize: '120px' }}/>
                 </div>
                 <div className="board_title">
                     <h2>NEW BOARD</h2>
