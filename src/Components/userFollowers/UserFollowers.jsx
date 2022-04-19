5c32bb972bbaddeca991474e42d5c06de9936ab2
ilir2523 <86475814+ilir2523@users.noreply.github.com>
2022-04-19 13:06:29 +0200

    follow following fixed

--- src/Components/userFollowers/UserFollowers.jsx
+++ src/Components/userFollowers/UserFollowers.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useStore } from '../../Store'
-import FollowingItem from '../Following/FollowingItem'
+import FollowingItem from '../Following/FollowersItem'
 
 export default function UserFollowers() {
     const [followers, setFollowers] = useState([])
+    const [following, setFollowing] = useState([])
+
     const user = useStore(store => store.user)
     const userFound = useStore(store => store.userFound)
     const setUserFollowsFunction = useStore(store => store.setUserFollowsFunction)
@@ -25,9 +27,21 @@ export default function UserFollowers() {
             })
     }, [userFound, user])
 
+    useEffect(() => {
+        fetch(`http://localhost:4001/getFollowing/${userFound?.id}`)
+            .then(resp => resp.json())
+            .then(data => {
+                if (data.error) {
+                    alert(data.error)
+                } else {
+                    setFollowing(data)
+                }
+            })
+    }, [userFound?.id])
+
     return (
         <div className='all-following'>
-            {followers.map(follower => <FollowingItem follow={follower} key={follower.id} />)}
+            {followers.map(follower => <FollowingItem follow={follower} key={follower.id} following={following} />)}
         </div>
     )
 }
