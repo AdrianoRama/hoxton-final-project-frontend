4b2b9643f99299f15a23d3e30069667c1ec8fecf
Arita Osmani <aritaaosmani@gmail.com>
2022-04-22 01:14:03 +0200

    ...

--- src/Components/BoardPage/Board.jsx
+++ src/Components/BoardPage/Board.jsx
@@ -14,6 +14,7 @@ export default function Board() {
     const [board, setBoard] = useState(false)
 
     const userFound = useStore(store => store.userFound)
+    const user = useStore(store => store.user)
     const userFoundCollections = useStore(store => store.userFoundCollections)
     const getUserByUsername = useStore(store => store.getUserByUsername)
 
@@ -28,14 +29,14 @@ export default function Board() {
         {board ? <CreateBoard setBoard={setBoard} /> : null}
 
         <div className="board__container">
-            <div className="board_box" onClick={() => { setBoard(true) }}>
+            {user?.id === userFound?.id ? <div className="board_box" onClick={() => { setBoard(true) }}>
                 <div className="board_addBoard">
                     <Add className='plus' style={{ fontSize: '120px' }} />
                 </div>
                 <div className="board_title">
                     <h2>NEW BOARD</h2>
                 </div>
-            </div>
+            </div> : null}
 
             {userFoundCollections?.map((collection) => {
                 return <BoardBox collection={collection} key={collection.id} />
